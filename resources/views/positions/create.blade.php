91fa569e03ecea602967a4642a1c20552df5db50
Yakanaro <kipaherd@gmail.com>
2023-06-01 04:11:34 +0300

    fix view create position

--- resources/views/positions/create.blade.php
+++ resources/views/positions/create.blade.php
@@ -24,7 +24,6 @@
                     <div>
                         <label for="labels" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Выберите метки</label>
                         <select multiple id="labels" name="labels[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-{{--                            <option selected>Выберите метки</option>--}}
                             @foreach($labels as $label)
                                 <option value="{{$label->id}}">{{$label->title}}</option>
                             @endforeach
@@ -34,7 +33,7 @@
                         <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                         <span class="sr-only">Info</span>
                         <div class="ml-3 text-sm font-medium">
-                            Для выбора нескольких меток зажмите Ctrl
+                            Для выбора нескольких меток зажмите <kbd class="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">Ctrl</kbd>
                         </div>
                         <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-1" aria-label="Close">
                             <span class="sr-only">Close</span>
