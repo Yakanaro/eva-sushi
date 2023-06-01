955bb7f285d80c74e1c359fe109ce3695dc4ffde
Yakanaro <kipaherd@gmail.com>
2023-06-01 20:10:37 +0300

    add home page on admin-panel

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 Route::get('admin', function () {
     return view('admin.adminIndex');
