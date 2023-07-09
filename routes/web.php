aa1323ed8db98fe6e7f44dbcc9a8c013dfbea348
Yakanaro <kipaherd@gmail.com>
2023-07-09 13:12:24 +0300

    Refactor routes with grouping for better readability

--- routes/web.php
+++ routes/web.php
@@ -31,38 +31,73 @@ Route::get('admin', function () {
 //    return view('admin.usersList');
 //})->name('admin.users');
 
-Route::get('admin/positions', [PositionController::class, 'index'])->name('admin.positions');
-Route::get('admin/positions/search', [PositionController::class, 'search'])->name('admin.search');
-Route::get('admin/positions/create', [PositionController::class, 'create'])->name('position.create');
-Route::post('admin/positions', [PositionController::class, 'store'])->name('position.store');
-Route::get('admin/positions/{position}/edit', [PositionController::class, 'edit'])->name('position.edit');
-Route::patch('admin/positions/{position}', [PositionController::class, 'update'])->name('position.update');
-Route::delete('admin/positions/{position}', [PositionController::class, 'destroy'])->name('position.delete');
+Route::prefix('admin/positions')->name('position.')->group(function () {
+    Route::get('/', [PositionController::class, 'index'])->name('index');
+    Route::get('/search', [PositionController::class, 'search'])->name('search');
+    Route::get('/create', [PositionController::class, 'create'])->name('create');
+    Route::post('/', [PositionController::class, 'store'])->name('store');
+    Route::get('/{position}/edit', [PositionController::class, 'edit'])->name('edit');
+    Route::patch('/{position}', [PositionController::class, 'update'])->name('update');
+    Route::delete('/{position}', [PositionController::class, 'destroy'])->name('delete');
+});
 
-Route::post('admin/categories', [CategoriesController::class, 'store'])->name('category.store');
-Route::get('admin/categories', [CategoriesController::class, 'index'])->name('category.index');
-Route::delete('admin/categories/{category}', [CategoriesController::class, 'destroy'])->name('category.delete');
-Route::get('admin/labels', [LabelController::class, 'index'])->name('label.index');
-Route::post('admin/labels', [LabelController::class, 'store'])->name('label.store');
-Route::delete('admin/labels/{label}', [LabelController::class, 'destroy'])->name('label.delete');
+Route::prefix('admin')->group(function () {
+    Route::get('users', [ProfileController::class, 'index'])->name('users.index');
 
-Route::post('/', [AddressController::class, 'store'])->name('address.store');
-Route::get('/addresses', [AddressController::class, 'index'])->name('address.index');
-Route::delete('/addresses/{address}', [AddressController::class, 'destroy'])->name('address.delete');
-Route::get('addresses/{address}/edit', [AddressController::class, 'edit'])->name('address.edit');
-Route::patch('addresses/{address}', [AddressController::class, 'update'])->name('address.update');
+    Route::post('categories', [CategoriesController::class, 'store'])->name('category.store');
+    Route::get('categories', [CategoriesController::class, 'index'])->name('category.index');
+    Route::delete('categories/{category}', [CategoriesController::class, 'destroy'])->name('category.delete');
 
-Route::post('/cart',[CartController::class, 'addToCart'])->name('cart.add');
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::delete('/cart/{position}', [CartController::class, 'destroy'])->name('cart.delete');
+    Route::post('labels', [LabelController::class, 'store'])->name('label.store');
+    Route::get('labels', [LabelController::class, 'index'])->name('label.index');
+    Route::delete('labels/{label}', [LabelController::class, 'destroy'])->name('label.delete');
+});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::post('/', [AddressController::class, 'store'])->name('address.store');
+//Route::get('/addresses', [AddressController::class, 'index'])->name('address.index');
+//Route::delete('/addresses/{address}', [AddressController::class, 'destroy'])->name('address.delete');
+//Route::get('addresses/{address}/edit', [AddressController::class, 'edit'])->name('address.edit');
+//Route::patch('addresses/{address}', [AddressController::class, 'update'])->name('address.update');
 
-Route::get('admin/users', [ProfileController::class, 'index'])->name('profile.index');
+Route::resource('addresses', AddressController::class)
+    ->except(['show', 'create'])
+    ->names([
+        'index' => 'address.index',
+        'store' => 'address.store',
+        'edit' => 'address.edit',
+        'update' => 'address.update',
+        'destroy' => 'address.delete'
+    ]);
 
-Route::get('/account', [AccountUserController::class, 'index'])->name('account.index');
+//Route::post('/cart',[CartController::class, 'addToCart'])->name('cart.add');
+//Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+//Route::delete('/cart/{position}', [CartController::class, 'destroy'])->name('cart.delete');
+
+Route::prefix('cart')->group(function () {
+    Route::post('/', [CartController::class, 'addToCart'])->name('cart.add');
+    Route::get('/', [CartController::class, 'index'])->name('cart.index');
+    Route::delete('/{position}', [CartController::class, 'destroy'])->name('cart.delete');
+});
+
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//Route::get('admin/users', [ProfileController::class, 'index'])->name('profile.index');
+//
+//Route::get('/account', [AccountUserController::class, 'index'])->name('account.index');
+
+//Route::prefix('admin')->name('admin.')->group(function () {
+//    Route::get('users', [ProfileController::class, 'index'])->name('users.index');
+//});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/account', [AccountUserController::class, 'index'])->name('account.index');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
