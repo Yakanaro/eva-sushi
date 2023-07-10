2fbd05bdf0efcf62fea253d2ef77d616fcd7b2d1
Yakanaro <kipaherd@gmail.com>
2023-07-10 17:05:26 +0300

    Add missing div tag to position index view

--- resources/views/positions/index.blade.php
+++ resources/views/positions/index.blade.php
@@ -80,6 +80,7 @@
                         <button type="submit" data-modal-target="edit-position-{{ $position->id }}" data-modal-toggle="edit-position-{{ $position->id }}" data-position-id="{{ $position->id }}" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Изменить</button>
                     </div>
                 </td>
+                </div>
             </tr>
         @endforeach
         </tbody>
