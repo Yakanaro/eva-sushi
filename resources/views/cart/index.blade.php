6d1c24e9399c8610fd90a8daf52cc0b834ef141d
Yakanaro <kipaherd@gmail.com>
2023-07-20 23:31:40 +0300

    Refactor code for guest quantity adjustment.

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -142,16 +142,16 @@
                 </div>
                 <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 text-center mt-2 ">Количество гостей</label>
                 <div class="flex items-center space-x-3 justify-center mb-3">
-                    <button class="quantity-decrease inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                    <button class="quantity-decrease-guests inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
                         <span class="sr-only">Quantity button</span>
                         <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
                         </svg>
                     </button>
                     <div>
-                        <input type="number" class="quantity-input bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required value="1">
+                        <input type="number" class="quantity-input-guests bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required value="1">
                     </div>
-                    <button class="quantity-increase inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                    <button class="quantity-increase-guests inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
                         <span class="sr-only">Quantity button</span>
                         <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
@@ -280,12 +280,40 @@
                                     let positionCount = document.getElementById("positionCount");
                                     positionCount.textContent = parseInt(positionCount.textContent) - 1;
                                 } else {
-                                    // Handle error here.
                                 }
                             }).catch((error) => {
                             console.error('Error:', error);
                         });
                     });
+                });
+                recalculateTotalCost();
+            });
+        </script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var decButton = document.querySelector('.quantity-decrease-guests');
+                var incButton = document.querySelector('.quantity-increase-guests');
+                var inputElement = document.querySelector('.quantity-input-guests');
+
+                var savedValue = sessionStorage.getItem('guests');
+                if(savedValue) {
+                    inputElement.value = savedValue;
+                }
+
+                decButton.addEventListener('click', function() {
+                    var value = parseInt(inputElement.value) || 1;
+                    if(value > 1) {
+                        value--;
+                    }
+                    inputElement.value = value;
+                    sessionStorage.setItem('guests', value);
+                });
+
+                incButton.addEventListener('click', function() {
+                    var value = parseInt(inputElement.value) || 1;
+                    value++;
+                    inputElement.value = value;
+                    sessionStorage.setItem('guests', value);
                 });
             });
         </script>
